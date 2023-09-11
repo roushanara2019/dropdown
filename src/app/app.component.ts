9550f17059d66e31eaa74b4712d61df00252afe7
ranjanr <ranjanr@opentext.com>
2023-09-11 12:56:30 +0530

    Dropdown data id updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,15 @@ export class AppComponent {
   title = 'dropdown';
   public dropdownData = [
     { id: 1, value: 'Option 1' },
-    { id: 1, value: 'Option 2' },
-    { id: 1, value: 'Option 3' },
-    { id: 1, value: 'Option 4' },
-    { id: 1, value: 'Option 5' },
-    { id: 1, value: 'Option 6' },
-    { id: 1, value: 'Option 7' },
-    { id: 1, value: 'Option 8' },
-    { id: 1, value: 'Option 9' },
-    { id: 1, value: 'Option 10' },
+    { id: 2, value: 'Option 2' },
+    { id: 3, value: 'Option 3' },
+    { id: 4, value: 'Option 4' },
+    { id: 5, value: 'Option 5' },
+    { id: 6, value: 'Option 6' },
+    { id: 7, value: 'Option 7' },
+    { id: 8, value: 'Option 8' },
+    { id: 9, value: 'Option 9' },
+    { id: 10, value: 'Option 10' },
   ];
   tooltip = 'Select an option';
   selected: any;
